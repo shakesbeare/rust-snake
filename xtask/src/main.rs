b93122c998b215fa1f414c4989c8df14a1b42101
Berint Moffett <berint.moffett@gmail.com>
2024-04-12 13:28:18 -0600

    Begin further automating deployment process

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -11,6 +11,8 @@ struct Cli {
 enum Subcommand {
     #[clap(name = "wasm-opt")]
     WasmOpt,
+    #[clap(name = "wasm-deploy")]
+    WasmDeploy
 }
 
 fn main() {
@@ -18,6 +20,7 @@ fn main() {
 
     let app_result = match app.cmd {
         Subcommand::WasmOpt => wasm_opt(),
+        Subcommand::WasmDeploy => wasm_deploy(),
     };
 
     if let Err(e) = app_result {
@@ -58,6 +61,39 @@ fn wasm_opt() -> anyhow::Result<()> {
         .wait()?;
 
     println!("xtask/wasm-opt => Done!");
+
+    Ok(())
+}
+
+fn wasm_deploy() -> anyhow::Result<()> {
+    println!("*** This command uses specific paths that might not work on other machines!");
+    println!("xtask/wasm-deploy => Building and optimizing wasm");
+    Command::new("cargo")
+        .args(["xtask", "wasm-opt"])
+        .spawn()?
+        .wait()?;
+
+    println!("xtask/wasm-deploy => Copying scripts to website...");
+    Command::new("cp")
+        .args([
+            "target/wasm32-unknown-unknown/release-wasm/opt/rust-snake.d.ts",
+            "target/wasm32-unknown-unknown/release-wasm/opt/rust-snake.js",
+            "target/wasm32-unknown-unknown/release-wasm/opt/rust-snake_bg.wasm.d.ts",
+            "/Users/bmoffett//projects/berintmoffett-com/main/client/static/wasm/rust-snake/",
+        ])
+        .spawn()?
+        .wait()?;
+
+    println!("xtask/wasm-deploy => Copying wasm file to Dropbox...");
+    Command::new("cp")
+        .args([
+            "target/wasm32-unknown-unknown/release-wasm/opt/rust-snake_bg.wasm",
+            "/Users/bmoffett/Dropbox/website/website-wasm/rust-snake_bg.wasm",
+        ])
+        .spawn()?
+        .wait()?;
+
+    println!("xtask/wasm-deploy => Automated tasks finished. Push changes on berintmoffett-com to github and restart server to complete");
 
     Ok(())
 }
